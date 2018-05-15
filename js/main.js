4a2e8a726c282e3327e97e9c54eb7909b298f771
Kevin Yee <kevinyee@utexas.edu>
2018-05-15 15:23:50 -0400

    adding About Me Page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // typing
 $(function () {
     $('.type').typed({
-        strings: ['deep learn in the cloud.', 'blockchain.', 'to use DP.', 'be noticed by senpai.'],
+        strings: ['deep learn in the cloud.', 'blockchain.', 'use DP.', 'breadth-first search.'],
         typeSpeed: 50,
         backSpeed: 50,
         backDelay: 1000,
@@ -11,3 +11,19 @@ $(function () {
 
 window.sr = ScrollReveal({ reset: true });
 sr.reveal('.section-title', { duration: 3000 });
+
+
+// Add Scroll Effects
+
+var current = 1; //keeps track of the current div
+var height = $('.roles').height(); //the height of the roles div
+var numberDivs = $('.roles').children().length; //the number of children of the roles div
+var first = $('.roles div:nth-child(1)'); //the first div nested in roles div
+setInterval(function() {
+    var number = current * -height;
+    first.css('margin-top', number + 'px');
+    if (current === numberDivs) {
+        first.css('margin-top', '0px');
+        current = 1;
+    } else current++;
+}, 2000);
